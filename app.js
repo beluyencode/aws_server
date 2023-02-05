62e69205ddbfac03e41c6a8747684e8c0ca0e98d
beluyencode <longlc956@gmail.com>
2023-02-05 23:07:30 +0700

    1

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
-const ip = require('ip');
+const { execSync } = require("child_process");
+const cmd = `curl -s http://checkip.amazonaws.com || printf "0.0.0.0"`;
+const pubIp = execSync(cmd).toString().trim();
+
+console.log();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on ${ip.address()}`)
+    console.log(`My public IP address is: ${pubIp}`)
 });
