1453d836ea335a24818a3fe405c8678110a12fff
beluyencode <longlc956@gmail.com>
2023-02-05 22:58:33 +0700

    1

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
+const ip = require('ip');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on ${ip.address()}`)
 });
