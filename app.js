24cb32bf24725b1919786f04283d3593ef6de6f3
beluyencode <longlc956@gmail.com>
2023-02-06 14:47:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const { execSync } = require("child_process");
 const cmd = `curl -s http://checkip.amazonaws.com || printf "0.0.0.0"`;
 const pubIp = execSync(cmd).toString().trim();
 
-console.log();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
